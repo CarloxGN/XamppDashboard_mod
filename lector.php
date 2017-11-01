7a115b3d95dd21a6b98955ab594fb76477b5e85c
CarloxGo <31928384+CarloxGN@users.noreply.github.com>
2017-11-01 08:41:06 -0400

    Add files via upload

--- lector.php
+++ lector.php
@@ -1,4 +1,17 @@
-<div class="large-12 columns">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <link rel="stylesheet" href="">
+    <link href='https://fonts.googleapis.com/css?family=Michroma' rel='stylesheet' />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="large-12 columns" style="font-family: Michroma;">
   <?php
   $directorio = opendir("../"); //ruta actual
   while ($archivo = readdir($directorio)){
@@ -11,14 +24,20 @@
       if(is_dir($archivo) === true){
             echo '<a target="_blank" href="../'.$archivo.'">['.$archivo.']</a><br />';
       }else{
+        if(!is_file($archivo)){
+          echo '<a target="_blank" href="../'.$archivo.'">['.$archivo.']</a><br />';
+        }else{
           $pos = strpos($archivo, '.');
           $largo = strlen($archivo);
           if ($pos == true && $largo >= 3){
               echo '<a target="_blank" href="../'.$archivo.'">'.$archivo.'</a><br />';
           }else{
               echo '<a target="_blank" href="../'.$archivo.'">['.$archivo.']</a><br />';
-          }           
+          }
+        }
       }
   }
 ?>
 </div>
+</body>
+</html>
